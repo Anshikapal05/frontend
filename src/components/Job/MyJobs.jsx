2b5424ca79a5f16d70f1fa15ea31304d8bc62721
Anshikapal05 <anshikapal546@gmail.com>
2024-12-12 22:07:35 +0530

    Initial commit

--- src/components/Job/MyJobs.jsx
+++ src/components/Job/MyJobs.jsx
@@ -17,7 +17,7 @@ const MyJobs = () => {
     const fetchJobs = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/job/getmyjobs",
+          "https://backend-ten-olive.vercel.app//api/v1/job/getmyjobs",
           { withCredentials: true }
         );
         setMyJobs(data.myJobs);
@@ -47,7 +47,7 @@ const MyJobs = () => {
   const handleUpdateJob = async (jobId) => {
     const updatedJob = myJobs.find((job) => job._id === jobId);
     await axios
-      .put(`http://localhost:4000/api/v1/job/update/${jobId}`, updatedJob, {
+      .put(`https://backend-ten-olive.vercel.app/api/v1/job/update/${jobId}`, updatedJob, {
         withCredentials: true,
       })
       .then((res) => {
